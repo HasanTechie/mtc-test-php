fbe93428ac09b51d2b4ba797d2a857076a2b9a10
hasanabbax <hasanabbax@gmail.com>
2019-10-24 03:17:39 +0000

    added .env support using Dotenv package; tested connection to MySQL server

---
+++ db.php
@@ -0,0 +1,10 @@
+<?php
+$dotenv = Dotenv\Dotenv::create(__DIR__);
+$dotenv->load();
+
+$connection = mysqli_connect(getenv('DB_HOST'), getenv('DB_USERNAME'), getenv('DB_PASSWORD'), getenv('DB_DATABASE'));
+
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+?>
